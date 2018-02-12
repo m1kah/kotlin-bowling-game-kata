4838899b28b490613e6a261bfd01ef137ef9388b
Mika Hamalainen <mika.hamalainen@gmail.com>
2018-02-12 16:12:31 +0200

    Complete kata

--- src/main/kotlin/bowling/Game.kt
+++ src/main/kotlin/bowling/Game.kt
@@ -1,11 +1,38 @@
 package bowling
 
 class Game {
-    fun rolls(pins: Int) {
+    var rolls = Array(21, { _ -> 0 })
+    var currentRoll = 0
 
+    fun roll(pins: Int) {
+        rolls[currentRoll++] = pins
     }
 
     fun score(): Int {
-        return 0
+        var score = 0
+        var frameIndex = 0
+        for (frame in 0..9) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex)
+                frameIndex++
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex)
+                frameIndex += 2
+            } else {
+                score += sumOfBallsInFrame(frameIndex)
+                frameIndex += 2
+            }
+        }
+        return score
     }
+
+    private fun sumOfBallsInFrame(frameIndex: Int) = rolls[frameIndex] + rolls[frameIndex + 1]
+
+    private fun strikeBonus(frameIndex: Int) = rolls[frameIndex + 1] + rolls[frameIndex + 2]
+
+    private fun spareBonus(frameIndex: Int) = rolls[frameIndex + 2]
+
+    private fun isStrike(frameIndex: Int) = rolls[frameIndex] == 10
+
+    private fun isSpare(frameIndex: Int) = rolls[frameIndex] + rolls[frameIndex + 1] == 10
 }
--- src/test/kotlin/bowling/KataTest.kt
+++ src/test/kotlin/bowling/KataTest.kt
@@ -1,12 +1,65 @@
 package bowling
 
+import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class KataTest {
-    var game = Game()
+    lateinit var g: Game
+
+    @Before
+    fun setup() {
+        g = Game()
+    }
 
     @Test
-    fun testGame() {
-        println("score: " + game.score())
+    fun testGutterGame() {
+        rollMany(20, 0)
+        assertEquals(0, g.score())
+    }
+
+    @Test
+    fun testAllOnes() {
+        rollMany(20, 1)
+        assertEquals(20, g.score())
+    }
+
+    @Test
+    fun testOneSpare() {
+        rollSpare()
+        g.roll(3)
+        rollMany(17, 0)
+        assertEquals(16, g.score())
+    }
+
+    @Test
+    fun testOneStrike() {
+        rollStrike()
+        g.roll(3)
+        g.roll(4)
+        rollMany(16, 0)
+        assertEquals(24, g.score())
+    }
+
+    @Test
+    fun testPerfectGame() {
+        rollMany(12, 10)
+        assertEquals(300, g.score())
+    }
+
+    private fun rollStrike() {
+        g.roll(10)
+    }
+
+    private fun rollMany(n: Int, pins: Int) {
+        for (i in 1..n) {
+            g.roll(pins)
+        }
+    }
+
+
+    private fun rollSpare() {
+        g.roll(5)
+        g.roll(5)
     }
 }
